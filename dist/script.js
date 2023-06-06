ba6fcdb40645e8fe8a80213126cc577c5b619686
Abraham MB Selebay <110640726+xyberkid@users.noreply.github.com>
2023-06-06 16:27:09 +0000

    hero page

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+// JavaScript for Toggle Button
+function toggleNavbar() {
+  let navbarLinks = document.querySelector(".navbar-links");
+  let toggleBtn = document.querySelector(".toggle-btn");
+
+  navbarLinks.classList.toggle("hidden");
+  toggleBtn.classList.toggle("active");
+
+}
+
+document.querySelector(".toggle-btn").addEventListener("click", function () {
+  toggleNavbar();
+});
+
+// slider
+  
