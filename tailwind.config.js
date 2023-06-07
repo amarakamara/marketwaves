c2caad25efebda0128bbe32fac6cdcaebd7b4fd8
omar <akamar5050@gmail.com>
2023-06-07 21:05:58 +0530

    started the cta, will continue😂
    cta started

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    colors: {
+      gold: "#d8ba75",
+      green: "#019267",
+      "light-green": "c1d8c8",
+      black: "#000000",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
