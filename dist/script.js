970e6fdf52ba262e6470d7677a9048dfc9123d79
omar <akamar5050@gmail.com>
2023-06-19 15:29:11 +0530

    Navbar done
    Navbar done

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,53 @@
 //Variables
+$(document).ready(function () {
+  const closeBtn = $(".close-btn");
+  const openBtn = $(".open-btn");
+  const myNav = $("#myNav");
 
+  function closeNav() {
+    myNav.css("width", "0");
+  }
+
+  function openNav() {
+    myNav.css("width", "100%");
+  }
+
+  closeBtn.on("click", closeNav);
+  openBtn.on("click", openNav);
+});
+
+/*
 const navbarLinks = $(".navbar-links");
 const toggleBtn = $(".toggle-btn");
 const navBar = $(".navbar");
+const navDiv = $(".nav-container");
+navItems = $(".nav-items");
 
 // JavaScript for Toggle Button
 function toggleNavbar() {
-  navbarLinks[0].classList.toggle("hidden");
+  navbarLinks[0].classList.toggle("active");
   toggleBtn[0].classList.toggle("active");
 }
 
 function changeNav() {
-  navBar[0].classList.toggle("old-navbar");
-  navBar[0].classList.toggle("new-navbar");
+  navbarLinks[0].style.display = "block";
+  //chanege container
+  navDiv[0].classList.toggle("old");
+  navDiv[0].classList.toggle("new");
+
+  //chanege navbar
+  navBar[0].classList.toggle("old-nav");
+  navBar[0].classList.toggle("new-nav");
+
+  //chanege navLinks
+  navbarLinks[0].classList.toggle("old-links");
+  navbarLinks[0].classList.toggle("new-links");
+
+  //change nav items
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].classList.toggle("inline");
+    navItems[i].classList.toggle("block");
+  }
 }
 
 document.querySelector(".toggle-btn").addEventListener("click", function () {
@@ -45,7 +80,7 @@ $(document).ready(function () {
     }
   });
 });
-
+*/
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   grabCursor: true,
