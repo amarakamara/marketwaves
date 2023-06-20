7d319e9dee38a06df54749bb7fadf31a5d5113ba
omar <akamar5050@gmail.com>
2023-06-20 14:00:47 +0530

    Main page almost completed
    Almost completed the main page

--- dist/script.js
+++ dist/script.js
@@ -68,3 +68,17 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//Form
+
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+window.onpopstate = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
